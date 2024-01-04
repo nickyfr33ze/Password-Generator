5300b4d554e97c8b7f77aac17ca8f1f316593907
Nick Friesen <nfriesen26@gmail.com>
2024-01-04 11:17:44 -0800

    Finished basic functionality. More to come...

---
+++ gen.py
@@ -0,0 +1,33 @@
+import random
+import sys
+import time
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=,./<>?;:[]\|' # All possible characters
+password = '' # The generated password
+length = int(input("Enter the desired password length: ")) # Get user input for password length
+
+if (length < 16): 
+    # If the password length is less than 16, print a warning
+    print("----------------------------------------")
+    print("Warning: Passwords shorter than 16 characters are not recommended.")
+    print("Please consider using a longer password.")
+    print("")
+    print("This program will now exit. Re-run 'python3 gen.py' to try again.")
+    print("----------------------------------------")
+    sys.exit() # Exit the program
+
+elif (length >= 16):
+    print("----------------------------------------")
+    print("Generating password...") # Print a message to let the user know the password is being generated
+    time.sleep(2) # Wait for 2 seconds
+    
+    for i in range(length): 
+        # Loop through the length of the password
+        password += random.choice(chars) # Add a random character to the password
+    print("\nPassword generated!") # Print a message to let the user know the password has been generated
+    print(password) # Print the password
+    print("")
+    print("Please consider using a password manager to store your password(s).") # Print a message asking the user to securely store their password.
+    print("----------------------------------------")
+
+else:
+    print("An error occurred. Please try again.") # Print an error message if something goes wrong
